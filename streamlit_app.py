20c23f1d9dab7764a0a11d261f8b94b2e750b7cb
Satya Chaitanya <66238780+bymeSatya@users.noreply.github.com>
2022-06-20 00:36:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,6 @@ import streamlit
 import pandas
 import requests
 import setuptools
-from azure.storage.blob import BlockBlobService
 import snowflake.connector
 
 streamlit.title('😎This is My streamlit Application')
