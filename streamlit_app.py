180d8837c046ca4e5579498e6c55d11775203be2
Satya Chaitanya <66238780+bymeSatya@users.noreply.github.com>
2022-06-19 13:04:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ fruity_responce=requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 fruity_normalize = pandas.json_normalize(fruity_responce.json())
 streamlit.dataframe(fruity_normalize)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
